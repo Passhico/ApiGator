5d0d8f92eeb9ab5c48e6ecf716a3b062e60b57ae
Pascual Muñoz Galián <pascual.munoz@pccomponentes.com>
2016-11-13 10:13:08 +0100

    skel listo.

--- express51.skel.php
+++ express51.skel.php
@@ -5,7 +5,9 @@ include 'ApiGator.php';
 use ApiGator\ApiGator;
 
 
-/* TODO: STUB Preparamos la closure que maneja la response de curl */
+/* TODO: Preparamos la closure(http://php.net/manual/es/functions.anonymous.php)
+ *  que maneja la response de curl */
+/* @var $PersistConversaciones Closure */
 $PersistConversaciones = function ($json) {
 	//implementación aquí:
 	var_dump(json_decode($json));
@@ -14,6 +16,4 @@ $PersistConversaciones = function ($json) {
 //Creamos el ApiGator de la sección que queramos y la closure se encarga de todo.
 $apiExpress51_Conversaciones = new ApiGator(SECTION_CONVERSACIONES);
 $apiExpress51_Conversaciones->procesaResponseCon($PersistConversaciones);
-
-
 
