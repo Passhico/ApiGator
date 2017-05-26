7babdd096cd3b24d2879f285ad0a88f411bb8dab
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2017-05-26 21:29:20 +0200

    borrandocachescoladas

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,22 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/test", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-
-        $APIKEY = '18a7c380835ade989f795f3298ab2b09';
-        $URI = 'api.openweathermap.org/data/2.5/weather?q=Murcia,es&APPID='. $APIKEY;
-
-        $apiGatorService = $this->get('pcc_apigator.apigator');
-
-        $apiGatorService->setCurrentUri($URI);
-        $decodedJson =  $apiGatorService->getJsonDecodedCurlResponse();
-
-        dump($decodedJson);
-        dump($this->get('pcc_apigator.apigator'));
-        dump(self::get('service_container'));
-    die();
+        return $this->redirectToRoute('pcc_apigator_testpage');
     }
 }
