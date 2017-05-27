d2b4e5992b1bb212e582836161cfc445f64308cb
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2017-05-27 11:28:00 +0200

    creada interfaz WeatherProviderInterface getMeasuresByCity(city)
    Creada clase WeatherProviderAPI que la implementa

---
+++ src/pcc/WeatherBundle/WeatherProviderAPI.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: passh
+ * Date: 27/5/17
+ * Time: 11:17
+ */
+
+namespace pcc\WeatherBundle;
+
+
+class WeatherProviderAPI implements WeatherProviderInterface
+{
+
+
+    public function getMeasuresByCity($city)
+    {
+
+    }
+}
--- src/pcc/WeatherBundle/WeatherProviderInterface.php
+++ src/pcc/WeatherBundle/WeatherProviderInterface.php
@@ -3,13 +3,14 @@
  * Created by PhpStorm.
  * User: passh
  * Date: 27/5/17
- * Time: 11:17
+ * Time: 11:22
  */
 
 namespace pcc\WeatherBundle;
 
 
-class weatherProvider
+interface WeatherProviderInterface
 {
+    public function getMeasuresByCity($city);
 
 }
